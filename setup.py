531a5b3f07c9a496ba144f9ab8b9e201a6984293
Dominik Heidler <dominik@heidler.eu>
2018-05-10 12:03:34 +0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 
 from distutils.core import setup
 
-from distutils.core import setup
-
 setup(
 	name='ripabook',
-	version='0.1',
+	version='0.2',
 	license='GPL',
 	description='Backend to ripp audiobooks',
 	author='Dominik Heidler',
